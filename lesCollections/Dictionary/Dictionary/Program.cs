ad08bef3e2080bfa1f494de2119cd7a4c2d72c46
emilieDelsol <emiliedelsol1@gmail.com>
2020-10-19 15:59:52 +0200

    add lists in dictionnary

--- lesCollections/Dictionary/Dictionary/Program.cs
+++ lesCollections/Dictionary/Dictionary/Program.cs
@@ -6,23 +6,47 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            Dictionary<char, string> dictionary = new Dictionary<char, string>();
 
-            dictionary["a"] = "alpha";
-            dictionary["b"] = "bravo";
-            dictionary["c"] = "charly";
+            dictionary['a'] = "alpha";
+            dictionary['b'] = "bravo";
+            dictionary['c'] = "charly";
             Console.WriteLine("My complete dictionnary: ");
             foreach(var value in dictionary) { Console.WriteLine($"Key: {value.Key}, value: {value.Value}"); };
             Console.WriteLine("\nMy complete dictionary with only value:");
             foreach (var value in dictionary) { Console.Write($" {value.Value}"); };
 
-            dictionary.Remove("c");
+            dictionary.Remove('c');
             Console.WriteLine("\nI have deleted letter 'c':");
             Console.WriteLine("\nMy complete dictionary is:");
             foreach (var value in dictionary) { Console.WriteLine($"Key: {value.Key}, value: {value.Value}"); };
 
             Console.WriteLine("\nMy complete dictionary with only value: ");
             foreach (var value in dictionary) { Console.Write($" {value.Value}"); };
+
+            Console.WriteLine("\n************\n");
+            Dictionary<char, List<string>> dictionnaryList = new Dictionary<char, List<string>>();
+            List<string> listeA = new List<string>() { "alpha", "around" };
+            dictionnaryList['a'] = listeA;
+            dictionnaryList['b'] = new List<string>() { "beta", "baba" };
+            dictionnaryList['c'] = new List<string>() { "charlie", "charles", "chat" };
+
+            foreach (var value in dictionnaryList)
+            {
+                Console.WriteLine($"\nDictionnaire à l'index {value.Key}");
+                foreach (string val in value.Value) { Console.Write($" {val} "); }
+            }
+
+            Console.WriteLine("\n\n************\nAfter remove index 'c'");
+            dictionnaryList.Remove('c');
+            foreach (var value in dictionnaryList)
+            {
+                Console.WriteLine($"\nDictionnaire à l'index {value.Key}");
+                foreach (string val in value.Value) 
+                { 
+                    Console.Write($" {val} "); 
+                }
+            }
         }
     }
 }
