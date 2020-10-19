bbe8df51f834a03d9e30554f5db5aff3b86f04b7
emilieDelsol <emiliedelsol1@gmail.com>
2020-10-19 11:46:35 +0200

    finish dictionary

---
+++ lesCollections/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            dictionary["a"] = "alpha";
+            dictionary["b"] = "bravo";
+            dictionary["c"] = "charly";
+            Console.WriteLine("My complete dictionnary: ");
+            foreach(var value in dictionary) { Console.WriteLine($"Key: {value.Key}, value: {value.Value}"); };
+            Console.WriteLine("\nMy complete dictionary with only value:");
+            foreach (var value in dictionary) { Console.Write($" {value.Value}"); };
+
+            dictionary.Remove("c");
+            Console.WriteLine("\nI have deleted letter 'c':");
+            Console.WriteLine("\nMy complete dictionary is:");
+            foreach (var value in dictionary) { Console.WriteLine($"Key: {value.Key}, value: {value.Value}"); };
+
+            Console.WriteLine("\nMy complete dictionary with only value: ");
+            foreach (var value in dictionary) { Console.Write($" {value.Value}"); };
+        }
+    }
+}
