2cee97b665ea8133b8abd8b2d981d5fbf0c558e9
emilieDelsol <emiliedelsol1@gmail.com>
2020-10-19 09:34:21 +0200

    create empty project listT

---
+++ lesCollections/listT/listT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace listT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
