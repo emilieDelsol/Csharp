80b2a3ebae2b2ecf8bfaa90517c76d2e792c9905
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-23 11:21:59 +0100

    add quete routing

---
+++ ASP.NET/HelloController/HelloController/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloController.Controllers
+{
+    public class HelloController : ControllerBase
+    {
+        [Route("hello/Say")]
+        [Route("hello/{action}")]
+
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+        [Route("hello/Yell")]
+        [Route("hello/{action}")]
+
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+
+        
+    }
+}
