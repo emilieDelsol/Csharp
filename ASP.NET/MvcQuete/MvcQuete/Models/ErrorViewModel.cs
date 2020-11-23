120bbe5a32306992bc8b21469d0886891cce929f
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-23 22:08:23 +0100

    enregistrement quête ASP.NETMVC, quete intergiciels et test tuto microsoft nommé queteMVC

---
+++ ASP.NET/MvcQuete/MvcQuete/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcQuete.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASP.NET/queteASP.NetMVC/queteASP.NetMVC/Models/Data/ArticleContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace queteASP.NetMVC.Models.Data
+{
+    public class ArticleContext : DbContext 
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Article> Article { get; set; }
+    }
+}
