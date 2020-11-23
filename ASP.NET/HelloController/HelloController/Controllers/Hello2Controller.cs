4cbd2a555dcb6bb442d25064edc00fa0491ebca3
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-23 18:37:57 +0100

    add new controller to ru 2 methods

---
+++ ASP.NET/HelloController/HelloController/Controllers/Hello2Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloController.Controllers
+{
+    public class HelloController : ControllerBase
+    {
+        public String SayHello()
+        {
+            return "Hello via conventionel !";
+        }
+
+        public String YellHello()
+        {
+            return "HELLO VIA CONVENTIONEL!";
+        }
+    }
+}
