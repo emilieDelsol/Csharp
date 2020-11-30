4ac4c8a49eaac38e4f1c681d9f7d68467f397bb3
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-30 19:02:34 +0100

    save quest Identity

---
+++ IdentityQuest/IdentityQuest/Controllers/userController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityQuest.Controllers
+{
+	[Authorize]
+	public class userController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
