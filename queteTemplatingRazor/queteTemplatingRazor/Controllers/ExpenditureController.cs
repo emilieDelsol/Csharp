791cab52a9cbe398a03f273c8f8d3d97026e2324
emilieDelsol <emiliedelsol1@gmail.com>
2020-12-07 11:29:28 +0100

    quete template razor

---
+++ queteTemplatingRazor/queteTemplatingRazor/Controllers/ExpenditureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using queteTemplatingRazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace queteTemplatingRazor.Controllers
+{
+	public class ExpenditureController : Controller
+	{
+		public IActionResult Index()
+		{
+			List<Expenditure> expenditures = new List<Expenditure>()
+			{
+				new Expenditure() { spentName = "achat nouveau siège", spentDate = DateTime.Now, spentPrice = 149 },
+				new Expenditure() { spentName = "achat jeux", spentDate = DateTime.Parse("12/10/2020 8:30:10 AM", System.Globalization.CultureInfo.InvariantCulture), spentPrice = 59 },
+				new Expenditure() { spentName = "achat cadeau", spentDate = DateTime.Parse("12/10/2020 8:30:10 AM", System.Globalization.CultureInfo.InvariantCulture), spentPrice = 99 },
+			};
+			return View(expenditures);
+		}
+	}
+}
