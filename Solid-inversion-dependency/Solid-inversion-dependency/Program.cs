6487df004256f1b7dc3ad6fc9c06fee60c2733cd
emilieDelsol <emiliedelsol1@gmail.com>
2020-12-30 19:06:17 +0100

    add inversion dependency

---
+++ Solid-inversion-dependency/Solid-inversion-dependency/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Solid_inversion_dependency
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+                Terminal terminal = new Terminal();
+
+                while (!terminal.Exited)
+
+                {
+
+                    Command command = terminal.PromptCommand();
+
+                    terminal.ExecuteCommand(command);
+
+                }
+
+        }
+	}
+}
---
+++ Solid-inversion-dependency/Solid-inversion-dependency/Terminal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Solid_inversion_dependency
+{
+	internal class Terminal
+	{
+		public bool Exited { get; internal set; }
+		private string _promptString;
+
+		public Terminal()
+		{
+			_promptString = String.Format("{0}$ ", System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+
+			Exited = false;
+		}
+		public Command PromptCommand()
+		{
+			string commandLine = Prompt();
+			return new Command(commandLine);
+		}
+		public string Prompt()
+		{
+			Console.Write(_promptString);
+			string userCommand = Console.ReadLine();
+			return userCommand;
+		}
+
+		public void ExecuteCommand(Command command)
+		{
+			try
+			{
+				command.Launch();
+				if (command.Output.Length > 0)
+				{
+					Console.WriteLine(command.Output);
+				}
+			}
+			catch (InvalidOperationException exception)
+			{
+				Console.Error.WriteLine("{0}: path not found", command);
+			}
+		}
+	}
+}
