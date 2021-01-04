5af5e658690d6fc180abd1afb1f25fda4c2fc2c3
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-04 10:55:53 +0100

    [+] add new quest refelxion c#

---
+++ ReflexionCSharp/ReflexionCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace ReflexionCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var reflected = new ReflectedClass();
+			var inspector = new InspectorClass(reflected);
+			inspector.GetAllProperties();
+			inspector.GetAllFields();
+			inspector.GetAllMethods();
+		}
+	}
+}
