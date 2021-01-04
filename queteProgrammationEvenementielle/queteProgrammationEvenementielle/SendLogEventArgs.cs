1ba40853d29f871b50dcb373016ed4725aa10b9b
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-04 17:30:37 +0100

    add quest programmation evenementielle

---
+++ queteProgrammationEvenementielle/queteProgrammationEvenementielle/SendLogEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace queteProgrammationEvenementielle
+{
+	class SendLogEventArgs : EventArgs
+    {
+        public String Message;
+        public DateTime DateTime;
+        public SendLogEventArgs(String message, DateTime dateTime)
+        {
+            Message = message;
+
+            DateTime = dateTime;
+        }
+
+    }
+}
---
+++ queteProgrammationEvenementielle/queteProgrammationEvenementielle/StandardOutputLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace queteProgrammationEvenementielle
+{
+	class StandardOutputLogger
+    {
+        public void Subscribe(Program program)
+        {
+            program.OnSendLog += OnLogSent;
+        }
+        public void OnLogSent(object sender, SendLogEventArgs args)
+        {
+            // When OnLogSent is called, it writes a log on the stream
+            HandleLogSent(args.Message, args.DateTime);
+        }
+        public void HandleLogSent(String message, DateTime? dateTime = null)
+        {
+            if (dateTime == null)
+            {
+                dateTime = DateTime.Now;
+            }
+            String formattedMessage = String.Format("{0} - {1}", dateTime, message);
+
+            Console.WriteLine(formattedMessage);
+
+        }
+    }
+}
